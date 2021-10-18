0b08e352e7ebc67c933cfd9ee1b44d65d06aee18
damien <91182145+damienmaier@users.noreply.github.com>
2021-10-19 00:58:34 +0200

    ajouté le fichier Int que j'avais oublié

---
+++ src/com/company/Int.java
@@ -0,0 +1,45 @@
+/*
+ * FILE-HEADER----------------
+ * Laboratoire : POO - labo4
+ * Fichier : Int.java
+ * Auteurs : Elliot Ganty et Damien Maier
+ * Date : 18.10.2021
+ * But : Le but est décrit dans les commentaires ci-dessous décrivant la classe de ce fichier
+ * Compilateur : OpenJDK 11
+ * ---------------------------
+ */
+
+package com.company;
+
+public class Int {
+    private int value;
+
+    static public void switchValues(Int first, Int second) {
+        int temp = first.value;
+        first.value = second.value;
+        second.value = temp;
+    }
+
+    public Int(int value) {
+        this.value = value;
+    }
+
+    public void switchValues(Int other) {
+        int temp = value;
+        value = other.value;
+        other.value = temp;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
